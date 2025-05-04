0a3b88a97aa408a3acc25728e76fa8f24ecff1f0
VeerShahCodes <shahveer1213@gmail.com>
2025-05-04 12:01:03 -0700

    make constructor for quickfind

--- UnionFind/UnionFind.cs
+++ UnionFind/UnionFind.cs
@@ -1,7 +1,54 @@
 ﻿namespace UnionFind
 {
-    public class UnionFind
-    {
+    public class QuickFind<T>
 
+    {
+        private int[] sets;
+        private Dictionary<T, int> map;
+
+        public QuickFind(IEnumerable<T> items)
+        {
+            sets = new int[items.Count()];
+            int i = 0;
+            foreach(var item in items)
+            {
+                sets[i] = item
+                i++;
+            }
+        }
+
+        public int Find(T p) => sets[map[p]];
+        public bool Union(T p, T q)
+        {
+            return false;
+        }
+        public bool AreConnected(T p, T q)
+        {
+            return false;
+        }
+    }
+
+    public class QuickUnion<T>
+    {
+        private int[] parents;
+        private Dictionary<T, int> map;
+
+        public QuickUnion(IEnumerable<T> items)
+        {
+
+        }
+
+        public int Find(T p)
+        {
+            return 0;
+        }
+        public bool Union(T p, T q)
+        {
+            return false;
+        }
+        public bool AreConnected(T p, T q)
+        {
+            return false;
+        }
     }
 }
