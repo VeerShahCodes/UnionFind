98883a37ac81b5c8b7ff1a61c1c04c06cf0d57c6
VeerShahCodes <shahveer1213@gmail.com>
2025-05-06 18:32:07 -0700

    tested find and union

--- TestProject2/Test1.cs
+++ TestProject2/Test1.cs
@@ -4,10 +4,17 @@
     [TestClass]
     public sealed class Test1
     {
+        QuickFind<int> qf = new QuickFind<int>(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
         [TestMethod]
-        public void TestMethod1()
+        public void TestFind()
         {
-            Assert.IsTrue(true);
+            Assert.IsTrue(qf.Find(2) == 1);
+        }
+
+        [TestMethod]
+        public void TestUnion()
+        {
+            Assert.IsTrue(qf.Union(1, 2) == true);
         }
     }
 }
